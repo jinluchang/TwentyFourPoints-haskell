2acf28fa4c3545b55d26fe8806c814a75384616e
Frank Waterret <waterret@gmail.com>
2011-10-08 03:09:15 -0400

    completely change algorithms
    	modified:   Main.hs

--- Main.hs
+++ Main.hs
@@ -1,113 +1,119 @@
 module Main where
 
-import List
+import Data.List
+import Control.Monad
 
-data Operator = Add | Minus | Multiply | Divide
+--
+-- Define Expressions
+--
+data Expr a = Number a
+            | Add (Expr a) (Expr a)
+            | Minus (Expr a) (Expr a)
+            | Multiply (Expr a) (Expr a)
+            | Divide (Expr a) (Expr a)
+            | Abs (Expr a)
+            | Signum (Expr a)
+            | Recip (Expr a)
+    deriving (Eq, Ord)
+
+instance (RealFrac a) => Num (Expr a) where
+    a + b = Add a b
+    a - b = Minus a b
+    a * b = Multiply a b
+    abs a = Abs a
+    signum a = Signum a
+    fromInteger x = Number $ fromInteger x
+
+instance (RealFrac a) => Fractional (Expr a) where
+    a / b = Divide a b
+    recip a = Recip a
+    fromRational x = Number $ fromRational x
+
+instance (Show a, RealFrac a) => Show (Expr a) where
+    showsPrec p (Number x) = showsPrec p $ (round x :: Integer)
+    showsPrec p (Add a b) = showParen (p>6) $
+        showsPrec 6 a . ('+':) . showsPrec 6 b
+    showsPrec p (Minus a b) = showParen (p>6) $
+        showsPrec 6 a . ('-':) . showsPrec 7 b
+    showsPrec p (Multiply a b) = showParen (p>7) $
+        showsPrec 7 a . ('*':) . showsPrec 7 b
+    showsPrec p (Divide a b) = showParen (p>7) $
+        showsPrec 7 a . ('/':) . showsPrec 8 b
+    showsPrec _ (Abs a) = ( "abs" ++ ) . showParen True (showsPrec 0 a)
+    showsPrec _ (Signum a) = ( "signum" ++ ) . showParen True (showsPrec 0 a)
+    showsPrec _ (Recip a) = ( "recip" ++ ) . showParen True (showsPrec 0 a)
+
+--
+-- Evaluate Expressions
+--
+eval :: (RealFrac a) => Expr a -> Maybe a
+eval (Number x) = return x
+eval (Add a b) = do
+    x <- eval a
+    y <- eval b
+    return $ x + y
+eval (Minus a b) = do
+    x <- eval a
+    y <- eval b
+    return $ x - y
+eval (Multiply a b) = do
+    x <- eval a
+    y <- eval b
+    return $ x * y
+eval (Divide a b) = do
+    x <- eval a
+    y <- eval b
+    case y of
+        0 -> fail "Divide by zero."
+        _ -> return $ x / y
+eval (Abs a) = liftM abs $ eval a
+eval (Signum a) = liftM signum $ eval a
+eval (Recip a) = liftM recip $ eval a
+
+--
+-- Build Expressions
+--
+buildExpr :: (RealFrac a) => [a] -> [Expr a]
+buildExpr [] = []
+buildExpr [x] = [Number x]
+buildExpr xs = concat $ iterate ( concat . map contract ) [es] !! ( len - 1 ) where
+    len = length es
+    es = map Number xs
+
+contract :: (RealFrac a) => [Expr a] -> [[Expr a]]
+contract xs = concat . map combine $ pickTwoFromList xs where
+    combine ( x , y , rs ) = map (:rs) $ cons x y
+
+cons :: (RealFrac a) => Expr a -> Expr a -> [Expr a]
+cons a b = [ a + b , a - b , a * b , a / b ]
+
+pickTwoFromList :: [a] -> [ ( a , a , [a] ) ]
+pickTwoFromList xs = concat $ map pickAgain $ pickOneFromList xs where
+    pickAgain ( y , ys ) = map combine $ pickOneFromList ys where
+        combine ( z , zs ) = ( y , z , zs )
+
+pickOneFromList :: [a] -> [ ( a , [a] ) ]
+pickOneFromList xs = pickGen id xs where
+    pickGen f [y] = [ ( y , f [] ) ]
+    pickGen f (y:ys) = ( y , f ys ) : pickGen ( f . (y:) ) ys
+    pickGen _ _ = error "pickGen"
+
+
+data Operator = Add' | Minus' | Multiply' | Divide'
     deriving (Show, Eq, Ord)
 data Token a = Op Operator | N a
     deriving (Show, Eq, Ord)
 
-tokens24 :: (Fractional a) => [a] -> [[Token a]]
-tokens24 nums = filter (\tokens -> Just 24 == calc tokens) $ stackGen nums
+expr24 :: (RealFrac a) => [a] -> [Expr a]
+expr24 nums = filter (\expr -> Just 24 == eval expr) $ buildExpr nums
 
 expressions24 :: (RealFrac a) => [a] -> [String]
-expressions24 nums = uniqueAfterSort . sort $ map prettyPrint $ tokens24 nums
+expressions24 nums = uniqueAfterSort . sort $ map show $ expr24 nums
 
 oneExpression24 :: (RealFrac a) => [a] -> Maybe String
 oneExpression24 nums = case expressions24 nums of
     x:_ -> Just x
     [] -> Nothing
-
-permutationsGen :: [a] -> [[a]]
-permutationsGen nums = map (map (nums !!)) $ permutations $ length nums where
-    permutations :: Int -> [[Int]]
-    permutations n | n == 0 = [[]]
-                   | otherwise = concat $ map (insertN n) $ permutations (n-1)
-    insertN :: Int -> [Int] -> [[Int]]
-    insertN n ns = map (\i -> take i ns ++ [n-1] ++ drop i ns) [0..(n-1)]
-
-calc :: (Fractional a) => [Token a] -> Maybe a
-calc xs = stackCalc xs []
-
-stackCalc :: (Fractional a) => [Token a] -> [a] -> Maybe a
-stackCalc [] [n] = Just n
-stackCalc (N n:xs) ns = stackCalc xs $ n:ns
-stackCalc (Op Add:xs) (a:b:ns) = stackCalc xs $ (a+b):ns
-stackCalc (Op Minus:xs) (a:b:ns) = stackCalc xs $ (a-b):ns
-stackCalc (Op Multiply:xs) (a:b:ns) = stackCalc xs $ (a*b):ns
-stackCalc (Op Divide:xs) (a:b:ns) | b == 0 = Nothing
-                                  | otherwise = stackCalc xs $ (a/b):ns
-stackCalc _ _ = undefined
-
-stackGen :: [a] -> [[Token a]]
-stackGen nums = map reverse $ concat $
-    map (\ns -> stackGenReverse ns 0 []) $ permutationsGen nums
-
-stackGenReverse :: [a] -> Int -> [Token a] -> [[Token a]]
-stackGenReverse [] 1 xs = [xs]
-stackGenReverse [] n xs = insertOperator [] n xs
-stackGenReverse nums n xs = insertNumber nums n xs ++ insertOperator nums n xs
-
-insertOperator :: [a] -> Int -> [Token a] -> [[Token a]]
-insertOperator nums n xs | n < 2 = []
-                         | otherwise = concat $
-    [ stackGenReverse nums (n-1) (Op Add:xs)
-    , stackGenReverse nums (n-1) (Op Minus:xs)
-    , stackGenReverse nums (n-1) (Op Multiply:xs)
-    , stackGenReverse nums (n-1) (Op Divide:xs)
-    ]
-
-insertNumber :: [a] -> Int -> [Token a] -> [[Token a]]
-insertNumber (num:nums) n xs = stackGenReverse nums (n+1) (N num:xs)
-insertNumber _ _ _ = undefined
-
-prettyPrintNums :: (RealFrac a) => [a] -> String
-prettyPrintNums nums = drop 1 $ concat $ map (\x -> "," ++ show (round x :: Int)) nums
-
-prettyPrint :: (RealFrac a) => [Token a] -> String
-prettyPrint tokens = prettyPrintStack tokens []
-
-prettyPrintStack :: (RealFrac a) => [Token a] -> [String] -> String
-prettyPrintStack [] [str] = str
-prettyPrintStack (N a:tokens) strs = prettyPrintStack tokens $ (show (round a :: Int)):strs
-prettyPrintStack (Op Add:tokens) (a:b:strs) = prettyPrintStack tokens $ (a ++ "+" ++ b):strs
-prettyPrintStack (Op Minus:tokens) (a:b:strs) = prettyPrintStack tokens $ (a ++ "-" ++ flipSign b):strs
-prettyPrintStack (Op Multiply:tokens) (a:b:strs) = prettyPrintStack tokens $ (multiplyParenthesis a ++ "*" ++ multiplyParenthesis b):strs
-prettyPrintStack (Op Divide:tokens) (a:b:strs) = prettyPrintStack tokens $ (multiplyParenthesis a ++ "/" ++ divideParenthesis b):strs
-prettyPrintStack _ _ = undefined
-
-flipSign :: String -> String
-flipSign xs = snd $ foldr flipSignChar (0, "") xs where
-    flipSignChar :: Char -> (Int, String) -> (Int, String)
-    flipSignChar x (n, ys) | x == '+' && n == 0 = (n, '-':ys)
-                           | x == '-' && n == 0 = (n, '+':ys)
-                           | x == '(' = (n+1, '(':ys)
-                           | x == ')' = (n-1, ')':ys)
-                           | otherwise = (n, x:ys)
-
-multiplyParenthesis :: String -> String
-multiplyParenthesis xs = if searchPlusOrMinus 0 xs
-    then "(" ++ xs ++ ")"
-    else xs
-
-divideParenthesis :: String -> String
-divideParenthesis xs = if searchPlusOrMinus 0 xs
-    then "(" ++ xs ++ ")"
-    else snd $ foldr flipMultiplyChar (0, "") xs where
-        flipMultiplyChar :: Char -> (Int, String) -> (Int, String)
-        flipMultiplyChar x (n, ys) | x == '*' && n == 0 = (n, '/':ys)
-                                   | x == '/' && n == 0 = (n, '*':ys)
-                                   | x == '(' = (n+1, '(':ys)
-                                   | x == ')' = (n-1, ')':ys)
-                                   | otherwise = (n, x:ys)
-
-searchPlusOrMinus :: Int -> String -> Bool
-searchPlusOrMinus 0 [] = False
-searchPlusOrMinus n (c:cs) | c == '(' = searchPlusOrMinus (n+1) cs
-                           | c == ')' = searchPlusOrMinus (n-1) cs
-                           | n == 0 && (c == '+' || c == '-') = True
-                           | otherwise = searchPlusOrMinus n cs
-searchPlusOrMinus n cs = error $ show n ++ " " ++ cs
 
 uniqueAfterSort :: (Eq a) => [a] -> [a]
 uniqueAfterSort xs = foldr addToList [] xs where
@@ -127,6 +133,9 @@ prints24 nums = prettyPrintNums nums ++ ": [" ++
     (concat $ map (\s -> "\n    " ++ s) expr) where
     expr = expressions24 nums
 
+prettyPrintNums :: (RealFrac a) => [a] -> String
+prettyPrintNums nums = drop 1 $ concat $ map (\x -> "," ++ show (round x :: Int)) nums
+
 numsGen :: (RealFrac a, Enum a) => Int -> a -> a -> [[a]]
 numsGen 1 minNum maxNum = map (\x -> [x]) [minNum..maxNum]
 numsGen n minNum maxNum | n > 1 = do
@@ -138,4 +147,3 @@ main :: IO ()
 main = do
     let result = map prints24 $ (numsGen 4 1 9 :: [[Rational]])
     mapM_ putStrLn result
-
